877553e78bf2719d873e7df5083d2752da162422
AngleSideAngle <asapaparo@gmail.com>
2022-07-18 00:18:29 -0400

    bot works

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::fmt::Write;
 use std::sync::Arc;
 
 use serenity::async_trait;
-use serenity::client::bridge::gateway::ShardManager;
-use serenity::framework::standard::macros::group;
-use serenity::framework::StandardFramework;
+use serenity::client::bridge::gateway::{ShardId, ShardManager};
+use serenity::framework::standard::buckets::{LimitedFor, RevertBucket};
+use serenity::framework::standard::macros::{check, command, group, help, hook};
+use serenity::framework::standard::{
+    help_commands,
+    Args,
+    CommandGroup,
+    CommandOptions,
+    CommandResult,
+    DispatchError,
+    HelpOptions,
+    Reason,
+    StandardFramework,
+};
 use serenity::http::Http;
+use serenity::model::channel::{Channel, Message};
 use serenity::model::event::ResumedEvent;
-use serenity::model::gateway::Ready;
+use serenity::model::gateway::{GatewayIntents, Ready};
+use serenity::model::id::UserId;
+use serenity::model::permissions::Permissions;
 use serenity::prelude::*;
-use tracing::{error, info};
+use serenity::utils::{content_safe, ContentSafeOptions};
+use tokio::sync::Mutex;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
-        info!("Connected as {}", ready.user.name);
+        println!("Connected as {}", ready.user.name);
     }
 
     async fn resume(&self, _: Context, _: ResumedEvent) {
-        info!("resumed");
+        println!("resumed");
     }
 }
+
+#[group]
+#[commands(test)]
+struct Game;
 
 #[tokio::main]
 async fn main() {
@@ -31,7 +51,7 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let token = env::var("TOKEN").expect("Expected a token in the environment");
     let http = Http::new(&token);
 
     let (owners, bot_id) = match http.get_current_application_info().await {
@@ -56,7 +76,8 @@ async fn main() {
             .with_whitespace(true)
             .on_mention(Some(bot_id))
             .prefix("+")
-            .owners(owners));
+            .owners(owners))
+        .group(&GAME_GROUP);
         // .before(f)
         // .after(f)
         // .normal_message(f)
@@ -73,4 +94,10 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+#[command]
+async fn test(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    msg.reply(&ctx, "sussy").await?;
+    Ok(())
 }
